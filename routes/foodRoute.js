00d6007524d585567cf5cae127e8a23b63f48702
Inspirational Scientificance <goutam893@email.com>
2026-08-18 15:52:39 +0530

    foodcontroller.js

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,16 +1,19 @@
 import express from "express";
-import { addFood, listFood, removeFood, updateFoodImages } from "../controllers/foodController.js";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
 import fs from "fs";
 
 const foodRouter = express.Router();
 
+
 const uploadDir = process.env.VERCEL ? "/tmp" : "uploads";
+
 
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
 
+// Image Storage Engine Configuration
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, uploadDir);
@@ -22,11 +25,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+// Endpoints
 foodRouter.post("/add", upload.single("image"), addFood);
 foodRouter.get("/list", listFood);
 foodRouter.post("/remove", removeFood);
-
-// one-time fix route
-foodRouter.get("/update-images", updateFoodImages);
 
 export default foodRouter;
